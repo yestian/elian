9183805e398393b079dc0b7904b1190920765ccd
yestian <yestian@qq.com>
2017-08-27 10:42:04 +0800

    compelte sql from more table

--- application/admin/controller/Agent.php
+++ application/admin/controller/Agent.php
@@ -1,14 +1,43 @@
 <?php
 namespace app\admin\controller;
-use think\Controller;
+use app\admin\controller\Common;
 
-class Agent extends Controller{
-	
+
+class Agent extends Common{
+	//代理商列表
 	public function lst(){
+		//先查询出关联表的统计数据，组成新表，以得到和主表的一对一关系
+		$sub1=db('agent_pay')->field('aid,sum(money) as allpay')->group('aid')->buildSql();
+		$sub2=db('agent_buy')->field('aid,sum(expense) as allbuy')->group('aid')->buildSql();
+		//然后再和主表join
+		$list=db('agent a')
+		->join($sub1.'b','a.id=b.aid','left')
+		->join($sub2.'c','a.id=c.aid','left')
+		->paginate(10);
+
+		$this->assign('list',$list);
 		return view();
 	}
 
+
+
+
+
+
+
+
+
+	//添加代理商
 	public function add(){
+		if(request()->isPost()){
+			$data=input('post.');
+			$res=db('agent')->insert($data);
+			if($res){
+				$this->success('添加代理商成功！','lst');
+			}else{
+				$this->error('添加代理商失败！');
+			}
+		}
 		return view();
 	}
 
@@ -18,25 +47,6 @@ class Agent extends Controller{
 
 	public function del(){
 		
-	}
-	public function detail(){
-		return view();
-	}
-
-	public function cuslst(){
-		return view();
-	}
-
-	public function orderlst(){
-		return view();
-	}
-
-	public function buyrecord(){
-		return view();
-	}
-
-	public function upload(){
-		return view();
 	}
 }
 
